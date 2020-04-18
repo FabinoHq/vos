f908ac6b3ba233b3f4a4ead58b72982522caef0e
FabinoHq <fabino.hq@gmail.com>
2020-04-18 19:25:41 +0200

    Add framebuffer constants

--- Renderer/OpenGL/OpenGL.h
+++ Renderer/OpenGL/OpenGL.h
@@ -149,6 +149,10 @@
     #undef  GL_MINOR_VERSION
     #undef  GL_NUM_EXTENSIONS
     #undef  GL_CONTEXT_FLAGS
+    #undef  GL_INDEX
+    #undef  GL_UNSIGNED_INT_24_8
+    #undef  GL_UNSIGNED_NORMALIZED
+    #undef  GL_SRGB
     #undef  GL_LOGIC_OP
     #undef  GL_LOGIC_OP_MODE
     #undef  GL_INDEX_LOGIC_OP
@@ -477,6 +481,8 @@
     #undef  GL_DEPTH_WRITEMASK
     #undef  GL_DEPTH_CLEAR_VALUE
     #undef  GL_DEPTH_FUNC
+    #undef  GL_DEPTH_STENCIL
+    #undef  GL_DEPTH24_STENCIL8
     #undef  GL_STENCIL_TEST
     #undef  GL_STENCIL_CLEAR_VALUE
     #undef  GL_STENCIL_FUNC
@@ -493,6 +499,10 @@
     #undef  GL_STENCIL_BACK_REF
     #undef  GL_STENCIL_BACK_VALUE_MASK
     #undef  GL_STENCIL_BACK_WRITEMASK
+    #undef  GL_STENCIL_INDEX1
+    #undef  GL_STENCIL_INDEX4
+    #undef  GL_STENCIL_INDEX8
+    #undef  GL_STENCIL_INDEX16
     #undef  GL_MATRIX_MODE
     #undef  GL_NORMALIZE
     #undef  GL_VIEWPORT
@@ -914,6 +924,7 @@
     #undef  GL_UNSIGNED_INT_SAMPLER_CUBE
     #undef  GL_UNSIGNED_INT_SAMPLER_1D_ARRAY
     #undef  GL_UNSIGNED_INT_SAMPLER_2D_ARRAY
+    #undef  GL_TEXTURE_STENCIL_SIZE
     #undef  GL_MAX_EVAL_ORDER
     #undef  GL_MAX_LIGHTS
     #undef  GL_MAX_CLIP_PLANES
@@ -947,6 +958,9 @@
     #undef  GL_MAX_TRANSFORM_FEEDBACK_SEPARATE_COMPONENTS
     #undef  GL_MAX_TRANSFORM_FEEDBACK_INTERLEAVED_COMPONENTS
     #undef  GL_MAX_TRANSFORM_FEEDBACK_SEPARATE_ATTRIBS
+    #undef  GL_MAX_RENDERBUFFER_SIZE
+    #undef  GL_MAX_COLOR_ATTACHMENTS
+    #undef  GL_MAX_SAMPLES
     #undef  GL_VERTEX_ARRAY
     #undef  GL_NORMAL_ARRAY
     #undef  GL_COLOR_ARRAY
@@ -1026,6 +1040,66 @@
     #undef  GL_FRAGMENT_SHADER_DERIVATIVE_HINT
     #undef  GL_SHADING_LANGUAGE_VERSION
     #undef  GL_CURRENT_PROGRAM
+    #undef  GL_INVALID_FRAMEBUFFER_OPERATION
+    #undef  GL_FRAMEBUFFER_ATTACHMENT_COLOR_ENCODING
+    #undef  GL_FRAMEBUFFER_ATTACHMENT_COMPONENT_TYPE
+    #undef  GL_FRAMEBUFFER_ATTACHMENT_RED_SIZE
+    #undef  GL_FRAMEBUFFER_ATTACHMENT_GREEN_SIZE
+    #undef  GL_FRAMEBUFFER_ATTACHMENT_BLUE_SIZE
+    #undef  GL_FRAMEBUFFER_ATTACHMENT_ALPHA_SIZE
+    #undef  GL_FRAMEBUFFER_ATTACHMENT_DEPTH_SIZE
+    #undef  GL_FRAMEBUFFER_ATTACHMENT_STENCIL_SIZE
+    #undef  GL_FRAMEBUFFER_DEFAULT
+    #undef  GL_FRAMEBUFFER_UNDEFINED
+    #undef  GL_DEPTH_STENCIL_ATTACHMENT
+    #undef  GL_DRAW_FRAMEBUFFER_BINDING
+    #undef  GL_FRAMEBUFFER_BINDING
+    #undef  GL_RENDERBUFFER_BINDING
+    #undef  GL_READ_FRAMEBUFFER
+    #undef  GL_DRAW_FRAMEBUFFER
+    #undef  GL_READ_FRAMEBUFFER_BINDING
+    #undef  GL_RENDERBUFFER_SAMPLES
+    #undef  GL_FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE
+    #undef  GL_FRAMEBUFFER_ATTACHMENT_OBJECT_NAME
+    #undef  GL_FRAMEBUFFER_ATTACHMENT_TEXTURE_LEVEL
+    #undef  GL_FRAMEBUFFER_ATTACHMENT_TEXTURE_CUBE_MAP_FACE
+    #undef  GL_FRAMEBUFFER_ATTACHMENT_TEXTURE_LAYER
+    #undef  GL_FRAMEBUFFER_COMPLETE
+    #undef  GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT
+    #undef  GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT
+    #undef  GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER
+    #undef  GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER
+    #undef  GL_FRAMEBUFFER_UNSUPPORTED
+    #undef  GL_COLOR_ATTACHMENT0
+    #undef  GL_COLOR_ATTACHMENT1
+    #undef  GL_COLOR_ATTACHMENT2
+    #undef  GL_COLOR_ATTACHMENT3
+    #undef  GL_COLOR_ATTACHMENT4
+    #undef  GL_COLOR_ATTACHMENT5
+    #undef  GL_COLOR_ATTACHMENT6
+    #undef  GL_COLOR_ATTACHMENT7
+    #undef  GL_COLOR_ATTACHMENT8
+    #undef  GL_COLOR_ATTACHMENT9
+    #undef  GL_COLOR_ATTACHMENT10
+    #undef  GL_COLOR_ATTACHMENT11
+    #undef  GL_COLOR_ATTACHMENT12
+    #undef  GL_COLOR_ATTACHMENT13
+    #undef  GL_COLOR_ATTACHMENT14
+    #undef  GL_COLOR_ATTACHMENT15
+    #undef  GL_DEPTH_ATTACHMENT
+    #undef  GL_STENCIL_ATTACHMENT
+    #undef  GL_FRAMEBUFFER
+    #undef  GL_RENDERBUFFER
+    #undef  GL_RENDERBUFFER_WIDTH
+    #undef  GL_RENDERBUFFER_HEIGHT
+    #undef  GL_RENDERBUFFER_INTERNAL_FORMAT
+    #undef  GL_RENDERBUFFER_RED_SIZE
+    #undef  GL_RENDERBUFFER_GREEN_SIZE
+    #undef  GL_RENDERBUFFER_BLUE_SIZE
+    #undef  GL_RENDERBUFFER_ALPHA_SIZE
+    #undef  GL_RENDERBUFFER_DEPTH_SIZE
+    #undef  GL_RENDERBUFFER_STENCIL_SIZE
+    #undef  GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE
 
 
     ////////////////////////////////////////////////////////////////////////////
@@ -1149,6 +1223,10 @@
     #define GL_MINOR_VERSION                                        0x821C
     #define GL_NUM_EXTENSIONS                                       0x821D
     #define GL_CONTEXT_FLAGS                                        0x821E
+    #define GL_INDEX                                                0x8222
+    #define GL_UNSIGNED_INT_24_8                                    0x84FA
+    #define GL_UNSIGNED_NORMALIZED                                  0x8C17
+    #define GL_SRGB                                                 0x8C40
 
     ////////////////////////////////////////////////////////////////////////////
     //  OpenGL logic constants                                                //
@@ -1545,6 +1623,8 @@
     #define GL_DEPTH_WRITEMASK                                      0x0B72
     #define GL_DEPTH_CLEAR_VALUE                                    0x0B73
     #define GL_DEPTH_FUNC                                           0x0B74
+    #define GL_DEPTH_STENCIL                                        0x84F9
+    #define GL_DEPTH24_STENCIL8                                     0x88F0
 
     ////////////////////////////////////////////////////////////////////////////
     //  OpenGL stencil constants                                              //
@@ -1565,6 +1645,10 @@
     #define GL_STENCIL_BACK_REF                                     0x8CA3
     #define GL_STENCIL_BACK_VALUE_MASK                              0x8CA4
     #define GL_STENCIL_BACK_WRITEMASK                               0x8CA5
+    #define GL_STENCIL_INDEX1                                       0x8D46
+    #define GL_STENCIL_INDEX4                                       0x8D47
+    #define GL_STENCIL_INDEX8                                       0x8D48
+    #define GL_STENCIL_INDEX16                                      0x8D49
 
     ////////////////////////////////////////////////////////////////////////////
     //  OpenGL matrices constants                                             //
@@ -1994,6 +2078,7 @@
     #define GL_UNSIGNED_INT_SAMPLER_CUBE                            0x8DD4
     #define GL_UNSIGNED_INT_SAMPLER_1D_ARRAY                        0x8DD6
     #define GL_UNSIGNED_INT_SAMPLER_2D_ARRAY                        0x8DD7
+    #define GL_TEXTURE_STENCIL_SIZE                                 0x88F1
 
     ////////////////////////////////////////////////////////////////////////////
     //  OpenGL min and max constants                                          //
@@ -2031,6 +2116,9 @@
     #define GL_MAX_TRANSFORM_FEEDBACK_SEPARATE_COMPONENTS           0x8C80
     #define GL_MAX_TRANSFORM_FEEDBACK_INTERLEAVED_COMPONENTS        0x8C8A
     #define GL_MAX_TRANSFORM_FEEDBACK_SEPARATE_ATTRIBS              0x8C8B
+    #define GL_MAX_RENDERBUFFER_SIZE                                0x84E8
+    #define GL_MAX_COLOR_ATTACHMENTS                                0x8CDF
+    #define GL_MAX_SAMPLES                                          0x8D57
 
     ////////////////////////////////////////////////////////////////////////////
     //  OpenGL arrays constants                                               //
@@ -2118,6 +2206,70 @@
     #define GL_FRAGMENT_SHADER_DERIVATIVE_HINT                      0x8B8B
     #define GL_SHADING_LANGUAGE_VERSION                             0x8B8C
     #define GL_CURRENT_PROGRAM                                      0x8B8D
+
+    ////////////////////////////////////////////////////////////////////////////
+    //  OpenGL framebuffer constants                                          //
+    ////////////////////////////////////////////////////////////////////////////
+    #define GL_INVALID_FRAMEBUFFER_OPERATION                        0x0506
+    #define GL_FRAMEBUFFER_ATTACHMENT_COLOR_ENCODING                0x8210
+    #define GL_FRAMEBUFFER_ATTACHMENT_COMPONENT_TYPE                0x8211
+    #define GL_FRAMEBUFFER_ATTACHMENT_RED_SIZE                      0x8212
+    #define GL_FRAMEBUFFER_ATTACHMENT_GREEN_SIZE                    0x8213
+    #define GL_FRAMEBUFFER_ATTACHMENT_BLUE_SIZE                     0x8214
+    #define GL_FRAMEBUFFER_ATTACHMENT_ALPHA_SIZE                    0x8215
+    #define GL_FRAMEBUFFER_ATTACHMENT_DEPTH_SIZE                    0x8216
+    #define GL_FRAMEBUFFER_ATTACHMENT_STENCIL_SIZE                  0x8217
+    #define GL_FRAMEBUFFER_DEFAULT                                  0x8218
+    #define GL_FRAMEBUFFER_UNDEFINED                                0x8219
+    #define GL_DEPTH_STENCIL_ATTACHMENT                             0x821A
+    #define GL_DRAW_FRAMEBUFFER_BINDING                             0x8CA6
+    #define GL_FRAMEBUFFER_BINDING                                  0x8CA6
+    #define GL_RENDERBUFFER_BINDING                                 0x8CA7
+    #define GL_READ_FRAMEBUFFER                                     0x8CA8
+    #define GL_DRAW_FRAMEBUFFER                                     0x8CA9
+    #define GL_READ_FRAMEBUFFER_BINDING                             0x8CAA
+    #define GL_RENDERBUFFER_SAMPLES                                 0x8CAB
+    #define GL_FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE                   0x8CD0
+    #define GL_FRAMEBUFFER_ATTACHMENT_OBJECT_NAME                   0x8CD1
+    #define GL_FRAMEBUFFER_ATTACHMENT_TEXTURE_LEVEL                 0x8CD2
+    #define GL_FRAMEBUFFER_ATTACHMENT_TEXTURE_CUBE_MAP_FACE         0x8CD3
+    #define GL_FRAMEBUFFER_ATTACHMENT_TEXTURE_LAYER                 0x8CD4
+    #define GL_FRAMEBUFFER_COMPLETE                                 0x8CD5
+    #define GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT                    0x8CD6
+    #define GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT            0x8CD7
+    #define GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER                   0x8CDB
+    #define GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER                   0x8CDC
+    #define GL_FRAMEBUFFER_UNSUPPORTED                              0x8CDD
+    #define GL_COLOR_ATTACHMENT0                                    0x8CE0
+    #define GL_COLOR_ATTACHMENT1                                    0x8CE1
+    #define GL_COLOR_ATTACHMENT2                                    0x8CE2
+    #define GL_COLOR_ATTACHMENT3                                    0x8CE3
+    #define GL_COLOR_ATTACHMENT4                                    0x8CE4
+    #define GL_COLOR_ATTACHMENT5                                    0x8CE5
+    #define GL_COLOR_ATTACHMENT6                                    0x8CE6
+    #define GL_COLOR_ATTACHMENT7                                    0x8CE7
+    #define GL_COLOR_ATTACHMENT8                                    0x8CE8
+    #define GL_COLOR_ATTACHMENT9                                    0x8CE9
+    #define GL_COLOR_ATTACHMENT10                                   0x8CEA
+    #define GL_COLOR_ATTACHMENT11                                   0x8CEB
+    #define GL_COLOR_ATTACHMENT12                                   0x8CEC
+    #define GL_COLOR_ATTACHMENT13                                   0x8CED
+    #define GL_COLOR_ATTACHMENT14                                   0x8CEE
+    #define GL_COLOR_ATTACHMENT15                                   0x8CEF
+    #define GL_DEPTH_ATTACHMENT                                     0x8D00
+    #define GL_STENCIL_ATTACHMENT                                   0x8D20
+    #define GL_FRAMEBUFFER                                          0x8D40
+    #define GL_RENDERBUFFER                                         0x8D41
+    #define GL_RENDERBUFFER_WIDTH                                   0x8D42
+    #define GL_RENDERBUFFER_HEIGHT                                  0x8D43
+    #define GL_RENDERBUFFER_INTERNAL_FORMAT                         0x8D44
+    #define GL_RENDERBUFFER_RED_SIZE                                0x8D50
+    #define GL_RENDERBUFFER_GREEN_SIZE                              0x8D51
+    #define GL_RENDERBUFFER_BLUE_SIZE                               0x8D52
+    #define GL_RENDERBUFFER_ALPHA_SIZE                              0x8D53
+    #define GL_RENDERBUFFER_DEPTH_SIZE                              0x8D54
+    #define GL_RENDERBUFFER_STENCIL_SIZE                            0x8D55
+    #define GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE                   0x8D56
 
 
     ////////////////////////////////////////////////////////////////////////////
