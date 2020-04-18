3f8448d87cdaedd03766937a257aa9ea003a4790
FabinoHq <fabino.hq@gmail.com>
2020-04-18 19:35:05 +0200

    Add uniform buffer constants

--- Renderer/OpenGL/OpenGL.h
+++ Renderer/OpenGL/OpenGL.h
@@ -961,6 +961,15 @@
     #undef  GL_MAX_RENDERBUFFER_SIZE
     #undef  GL_MAX_COLOR_ATTACHMENTS
     #undef  GL_MAX_SAMPLES
+    #undef  GL_MAX_VERTEX_UNIFORM_BLOCKS
+    #undef  GL_MAX_GEOMETRY_UNIFORM_BLOCKS
+    #undef  GL_MAX_FRAGMENT_UNIFORM_BLOCKS
+    #undef  GL_MAX_COMBINED_UNIFORM_BLOCKS
+    #undef  GL_MAX_UNIFORM_BUFFER_BINDINGS
+    #undef  GL_MAX_UNIFORM_BLOCK_SIZE
+    #undef  GL_MAX_COMBINED_VERTEX_UNIFORM_COMPONENTS
+    #undef  GL_MAX_COMBINED_GEOMETRY_UNIFORM_COMPONENTS
+    #undef  GL_MAX_COMBINED_FRAGMENT_UNIFORM_COMPONENTS
     #undef  GL_VERTEX_ARRAY
     #undef  GL_NORMAL_ARRAY
     #undef  GL_COLOR_ARRAY
@@ -1106,6 +1115,30 @@
     #undef  GL_MAP_INVALIDATE_BUFFER_BIT
     #undef  GL_MAP_FLUSH_EXPLICIT_BIT
     #undef  GL_MAP_UNSYNCHRONIZED_BIT
+    #undef  GL_UNIFORM_BUFFER
+    #undef  GL_UNIFORM_BUFFER_BINDING
+    #undef  GL_UNIFORM_BUFFER_START
+    #undef  GL_UNIFORM_BUFFER_SIZE
+    #undef  GL_UNIFORM_BUFFER_OFFSET_ALIGNMENT
+    #undef  GL_ACTIVE_UNIFORM_BLOCK_MAX_NAME_LENGTH
+    #undef  GL_ACTIVE_UNIFORM_BLOCKS
+    #undef  GL_UNIFORM_TYPE
+    #undef  GL_UNIFORM_SIZE
+    #undef  GL_UNIFORM_NAME_LENGTH
+    #undef  GL_UNIFORM_BLOCK_INDEX
+    #undef  GL_UNIFORM_OFFSET
+    #undef  GL_UNIFORM_ARRAY_STRIDE
+    #undef  GL_UNIFORM_MATRIX_STRIDE
+    #undef  GL_UNIFORM_IS_ROW_MAJOR
+    #undef  GL_UNIFORM_BLOCK_BINDING
+    #undef  GL_UNIFORM_BLOCK_DATA_SIZE
+    #undef  GL_UNIFORM_BLOCK_NAME_LENGTH
+    #undef  GL_UNIFORM_BLOCK_ACTIVE_UNIFORMS
+    #undef  GL_UNIFORM_BLOCK_ACTIVE_UNIFORM_INDICES
+    #undef  GL_UNIFORM_BLOCK_REFERENCED_BY_VERTEX_SHADER
+    #undef  GL_UNIFORM_BLOCK_REFERENCED_BY_GEOMETRY_SHADER
+    #undef  GL_UNIFORM_BLOCK_REFERENCED_BY_FRAGMENT_SHADER
+    #undef  GL_INVALID_INDEX
 
 
     ////////////////////////////////////////////////////////////////////////////
@@ -2125,6 +2158,15 @@
     #define GL_MAX_RENDERBUFFER_SIZE                                0x84E8
     #define GL_MAX_COLOR_ATTACHMENTS                                0x8CDF
     #define GL_MAX_SAMPLES                                          0x8D57
+    #define GL_MAX_VERTEX_UNIFORM_BLOCKS                            0x8A2B
+    #define GL_MAX_GEOMETRY_UNIFORM_BLOCKS                          0x8A2C
+    #define GL_MAX_FRAGMENT_UNIFORM_BLOCKS                          0x8A2D
+    #define GL_MAX_COMBINED_UNIFORM_BLOCKS                          0x8A2E
+    #define GL_MAX_UNIFORM_BUFFER_BINDINGS                          0x8A2F
+    #define GL_MAX_UNIFORM_BLOCK_SIZE                               0x8A30
+    #define GL_MAX_COMBINED_VERTEX_UNIFORM_COMPONENTS               0x8A31
+    #define GL_MAX_COMBINED_GEOMETRY_UNIFORM_COMPONENTS             0x8A32
+    #define GL_MAX_COMBINED_FRAGMENT_UNIFORM_COMPONENTS             0x8A33
 
     ////////////////////////////////////////////////////////////////////////////
     //  OpenGL arrays constants                                               //
@@ -2286,6 +2328,34 @@
     #define GL_MAP_INVALIDATE_BUFFER_BIT                            0x0008
     #define GL_MAP_FLUSH_EXPLICIT_BIT                               0x0010
     #define GL_MAP_UNSYNCHRONIZED_BIT                               0x0020
+
+    ////////////////////////////////////////////////////////////////////////////
+    //  OpenGL uniform buffer constants                                       //
+    ////////////////////////////////////////////////////////////////////////////
+    #define GL_UNIFORM_BUFFER                                       0x8A11
+    #define GL_UNIFORM_BUFFER_BINDING                               0x8A28
+    #define GL_UNIFORM_BUFFER_START                                 0x8A29
+    #define GL_UNIFORM_BUFFER_SIZE                                  0x8A2A
+    #define GL_UNIFORM_BUFFER_OFFSET_ALIGNMENT                      0x8A34
+    #define GL_ACTIVE_UNIFORM_BLOCK_MAX_NAME_LENGTH                 0x8A35
+    #define GL_ACTIVE_UNIFORM_BLOCKS                                0x8A36
+    #define GL_UNIFORM_TYPE                                         0x8A37
+    #define GL_UNIFORM_SIZE                                         0x8A38
+    #define GL_UNIFORM_NAME_LENGTH                                  0x8A39
+    #define GL_UNIFORM_BLOCK_INDEX                                  0x8A3A
+    #define GL_UNIFORM_OFFSET                                       0x8A3B
+    #define GL_UNIFORM_ARRAY_STRIDE                                 0x8A3C
+    #define GL_UNIFORM_MATRIX_STRIDE                                0x8A3D
+    #define GL_UNIFORM_IS_ROW_MAJOR                                 0x8A3E
+    #define GL_UNIFORM_BLOCK_BINDING                                0x8A3F
+    #define GL_UNIFORM_BLOCK_DATA_SIZE                              0x8A40
+    #define GL_UNIFORM_BLOCK_NAME_LENGTH                            0x8A41
+    #define GL_UNIFORM_BLOCK_ACTIVE_UNIFORMS                        0x8A42
+    #define GL_UNIFORM_BLOCK_ACTIVE_UNIFORM_INDICES                 0x8A43
+    #define GL_UNIFORM_BLOCK_REFERENCED_BY_VERTEX_SHADER            0x8A44
+    #define GL_UNIFORM_BLOCK_REFERENCED_BY_GEOMETRY_SHADER          0x8A45
+    #define GL_UNIFORM_BLOCK_REFERENCED_BY_FRAGMENT_SHADER          0x8A46
+    #define GL_INVALID_INDEX                                        0xFFFFFFFFu
 
 
     ////////////////////////////////////////////////////////////////////////////
