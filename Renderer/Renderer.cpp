c1302dd4eea6e576b25eebd335467128e47c77f8
FabinoHq <fabino.hq@gmail.com>
2020-08-17 14:47:47 +0200

    Fix index buffer type

--- Renderer/Renderer.cpp
+++ Renderer/Renderer.cpp
@@ -2270,7 +2270,7 @@ bool Renderer::createVertexBuffer()
 
     if (!m_indexBuffer.createBuffer(
         m_physicalDevice, m_vulkanDevice,
-        VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+        VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
         VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT))
     {
         // Could not create index buffer
