64ea224f12bd9cf21c21f4f636df8b6fa9237276
FabinoHq <fabino.hq@gmail.com>
2020-08-26 22:44:57 +0200

    Update VkQueryControlFlagBits

--- Renderer/Vulkan.h
+++ Renderer/Vulkan.h
@@ -1265,7 +1265,8 @@
     ////////////////////////////////////////////////////////////////////////////
     enum VkQueryControlFlagBits
     {
-        VK_QUERY_CONTROL_PRECISE_BIT = 0x00000001
+        VK_QUERY_CONTROL_PRECISE_BIT = 0x00000001,
+        VK_QUERY_CONTROL_FLAG_BITS_MAX_ENUM = 0x7FFFFFFF
     };
     typedef VkFlags VkQueryControlFlags;
 
