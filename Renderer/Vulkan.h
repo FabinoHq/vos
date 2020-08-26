5ba07ef8e232a0337d1dffd617d8b638b0dfeac7
FabinoHq <fabino.hq@gmail.com>
2020-08-26 21:59:12 +0200

    Update VkFilter

--- Renderer/Vulkan.h
+++ Renderer/Vulkan.h
@@ -670,6 +670,7 @@
     {
         VK_FILTER_NEAREST = 0,
         VK_FILTER_LINEAR = 1,
+        VK_FILTER_CUBIC_IMG = 1000015000,
         VK_FILTER_BEGIN_RANGE = 0,
         VK_FILTER_END_RANGE = 1,
         VK_FILTER_RANGE_SIZE = 2,
