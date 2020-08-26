4a76dace61e55028c56e2898641fa61248f62a28
FabinoHq <fabino.hq@gmail.com>
2020-08-26 22:27:24 +0200

    Update VkSparseMemoryBindFlagBits

--- Renderer/Vulkan.h
+++ Renderer/Vulkan.h
@@ -971,7 +971,8 @@
     ////////////////////////////////////////////////////////////////////////////
     enum VkSparseMemoryBindFlagBits
     {
-        VK_SPARSE_MEMORY_BIND_METADATA_BIT = 0x00000001
+        VK_SPARSE_MEMORY_BIND_METADATA_BIT = 0x00000001,
+        VK_SPARSE_MEMORY_BIND_FLAG_BITS_MAX_ENUM = 0x7FFFFFFF
     };
     typedef VkFlags VkSparseMemoryBindFlags;
 
