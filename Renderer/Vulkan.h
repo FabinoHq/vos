90c2cd53d21b29f2c0755f47c7392cfc1bb2ddac
FabinoHq <fabino.hq@gmail.com>
2020-08-26 22:38:25 +0200

    Update VkCullModeFlagBits

--- Renderer/Vulkan.h
+++ Renderer/Vulkan.h
@@ -1140,7 +1140,8 @@
         VK_CULL_MODE_NONE = 0,
         VK_CULL_MODE_FRONT_BIT = 0x00000001,
         VK_CULL_MODE_BACK_BIT = 0x00000002,
-        VK_CULL_MODE_FRONT_AND_BACK = 0x3
+        VK_CULL_MODE_FRONT_AND_BACK = 0x00000003,
+        VK_CULL_MODE_FLAG_BITS_MAX_ENUM = 0x7FFFFFFF
     };
     typedef VkFlags VkCullModeFlags;
     typedef VkFlags VkPipelineMultisampleStateCreateFlags;
