2a995b687985275a65b743f1817db4b219ea61b4
FabinoHq <fabino.hq@gmail.com>
2020-08-26 22:45:20 +0200

    Update VkCommandBufferResetFlagBits

--- Renderer/Vulkan.h
+++ Renderer/Vulkan.h
@@ -1275,7 +1275,8 @@
     ////////////////////////////////////////////////////////////////////////////
     enum VkCommandBufferResetFlagBits
     {
-        VK_COMMAND_BUFFER_RESET_RELEASE_RESOURCES_BIT = 0x00000001
+        VK_COMMAND_BUFFER_RESET_RELEASE_RESOURCES_BIT = 0x00000001,
+        VK_COMMAND_BUFFER_RESET_FLAG_BITS_MAX_ENUM = 0x7FFFFFFF
     };
     typedef VkFlags VkCommandBufferResetFlags;
 
