af44c877ec80a11c504bcfff3e1b08dcb1d91b58
FabinoHq <fabino.hq@gmail.com>
2020-08-26 22:33:53 +0200

    Update VkQueryResultFlagBits

--- Renderer/Vulkan.h
+++ Renderer/Vulkan.h
@@ -1053,11 +1053,13 @@
     ////////////////////////////////////////////////////////////////////////////
     //  VkQueryResultFlags                                                    //
     ////////////////////////////////////////////////////////////////////////////
-    enum VkQueryResultFlagBits {
+    enum VkQueryResultFlagBits
+    {
         VK_QUERY_RESULT_64_BIT = 0x00000001,
         VK_QUERY_RESULT_WAIT_BIT = 0x00000002,
         VK_QUERY_RESULT_WITH_AVAILABILITY_BIT = 0x00000004,
-        VK_QUERY_RESULT_PARTIAL_BIT = 0x00000008
+        VK_QUERY_RESULT_PARTIAL_BIT = 0x00000008,
+        VK_QUERY_RESULT_FLAG_BITS_MAX_ENUM = 0x7FFFFFFF
     };
     typedef VkFlags VkQueryResultFlags;
 
