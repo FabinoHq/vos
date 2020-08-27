818dd851f8b4caedcc3e1743caebe770b5bbdc5b
FabinoHq <fabino.hq@gmail.com>
2020-08-27 14:48:24 +0200

    Add VkPeerMemoryFeatureFlags

--- Renderer/Vulkan.h
+++ Renderer/Vulkan.h
@@ -1456,6 +1456,19 @@
     };
     typedef VkFlags VkSubgroupFeatureFlags;
 
+    ////////////////////////////////////////////////////////////////////////////
+    //  VkPeerMemoryFeatureFlags                                              //
+    ////////////////////////////////////////////////////////////////////////////
+    enum VkPeerMemoryFeatureFlagBits
+    {
+        VK_PEER_MEMORY_FEATURE_COPY_SRC_BIT = 0x00000001,
+        VK_PEER_MEMORY_FEATURE_COPY_DST_BIT = 0x00000002,
+        VK_PEER_MEMORY_FEATURE_GENERIC_SRC_BIT = 0x00000004,
+        VK_PEER_MEMORY_FEATURE_GENERIC_DST_BIT = 0x00000008,
+        VK_PEER_MEMORY_FEATURE_FLAG_BITS_MAX_ENUM = 0x7FFFFFFF
+    };
+    typedef VkFlags VkPeerMemoryFeatureFlags;
+
 
     ////////////////////////////////////////////////////////////////////////////
     //  VkApplicationInfo data structure                                      //
