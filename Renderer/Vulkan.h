e5c526123ea8836248dd1869c8da8d3c37f403da
FabinoHq <fabino.hq@gmail.com>
2020-08-27 14:42:14 +0200

    Add VkSamplerYcbcrModelConversion

--- Renderer/Vulkan.h
+++ Renderer/Vulkan.h
@@ -857,6 +857,22 @@
         VK_TESSELLATION_DOMAIN_ORIGIN_MAX_ENUM = 0x7FFFFFFF
     };
 
+    ////////////////////////////////////////////////////////////////////////////
+    //  VkSamplerYcbcrModelConversion enum                                    //
+    ////////////////////////////////////////////////////////////////////////////
+    enum VkSamplerYcbcrModelConversion
+    {
+        VK_SAMPLER_YCBCR_MODEL_CONVERSION_RGB_IDENTITY = 0,
+        VK_SAMPLER_YCBCR_MODEL_CONVERSION_YCBCR_IDENTITY = 1,
+        VK_SAMPLER_YCBCR_MODEL_CONVERSION_YCBCR_709 = 2,
+        VK_SAMPLER_YCBCR_MODEL_CONVERSION_YCBCR_601 = 3,
+        VK_SAMPLER_YCBCR_MODEL_CONVERSION_YCBCR_2020 = 4,
+        VK_SAMPLER_YCBCR_MODEL_CONVERSION_BEGIN_RANGE = 0,
+        VK_SAMPLER_YCBCR_MODEL_CONVERSION_END_RANGE = 4,
+        VK_SAMPLER_YCBCR_MODEL_CONVERSION_RANGE_SIZE = 5,
+        VK_SAMPLER_YCBCR_MODEL_CONVERSION_MAX_ENUM = 0x7FFFFFFF
+    };
+
 
     ////////////////////////////////////////////////////////////////////////////
     //  Vulkan flags definitions                                              //
